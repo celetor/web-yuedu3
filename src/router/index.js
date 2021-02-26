4a8f3515ad20bb1d6fe8b2052d6c5406fcb71838
Celeter <48249130+Celeter@users.noreply.github.com>
2021-02-26 11:52:02 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
+const originalPush = VueRouter.prototype.push
+
+VueRouter.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 const routes = [
   {
     path: "/",
