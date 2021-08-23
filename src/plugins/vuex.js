3a65fb491ea4ec3922e244c0ad45117bb9dad340
letterk <kissoul@126.com>
2021-08-24 03:44:06 +0800

    补漏

--- src/plugins/vuex.js
+++ src/plugins/vuex.js
@@ -10,6 +10,7 @@ export default new Vuex.Store({
     newConnect: true,
     shelf: [],
     catalog: "",
+    length: 0,
     readingBook: {},
     popCataVisible: false,
     contentLoading: true,
