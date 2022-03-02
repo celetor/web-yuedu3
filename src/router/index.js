eb4c0d87a1e84ee246e13fd6e0cb6d7f6007b941
kunfei <gekunfei@live.com>
2022-03-02 19:30:39 +0800

    优化

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-const originalPush = VueRouter.prototype.push
+const originalPush = VueRouter.prototype.push;
 
-VueRouter.prototype.push = function push (location) {
-  return originalPush.call(this, location).catch(err => err)
-}
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+};
 
 const routes = [
   {
