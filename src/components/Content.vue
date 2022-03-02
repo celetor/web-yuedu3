b8c2a7d7985cab2b82a7245e4176470c1b88c366
Xwite <82232510+Xwite@users.noreply.github.com>
2022-03-02 21:03:52 +0800

    fix:proxyImage not defined

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -33,7 +33,7 @@ export default {
       return this.$store.state.config.fontSize + "px";
     }
   },
-  method: {
+  methods: {
     proxyImage(content) {
       let imgPattern = /<img[^>]*src="([^"]*(?:"[^>]+\})?)"[^>]*>/;
       if (!imgPattern.test(content)) {
